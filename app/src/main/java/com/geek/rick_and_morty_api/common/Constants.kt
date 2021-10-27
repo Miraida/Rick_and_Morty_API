bc20ad63aa7027c9a1a59eee6fbee1c8b322ac22
Miraida <miraidajaanbaeva@gmail.com>
2021-10-28 00:13:55 +0600

    add keys for bundles into Constants.kt

--- app/src/main/java/com/geek/rick_and_morty_api/common/Constants.kt
+++ app/src/main/java/com/geek/rick_and_morty_api/common/Constants.kt
@@ -1,5 +1,7 @@
 package com.geek.rick_and_morty_api.common
 
 object Constants {
-
+    const val character_id_key = "kdfjlskjfslkfjs"
+    const val episode_id_key = "kdfjlskjfkdfjgsslkfjs"
+    const val location_id_key = "kdfjlskjfsl98578934kfjs"
 }
