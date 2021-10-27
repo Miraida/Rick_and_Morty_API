8ee654421dd4474547abc9a35704f6b3bd0f70ae
Miraida <miraidajaanbaeva@gmail.com>
2021-10-28 00:09:29 +0600

    add functions for detail fragments on domain package

--- app/src/main/java/com/geek/rick_and_morty_api/domain/model/Episodes.kt
+++ app/src/main/java/com/geek/rick_and_morty_api/domain/model/Episodes.kt
@@ -2,5 +2,6 @@ package com.geek.rick_and_morty_api.domain.model
 
 data class Episodes(
     val name: String?,
-    val air_date: String?
+    val air_date: String?,
+    val id:Int?
 )
--- app/src/main/java/com/geek/rick_and_morty_api/domain/model/Locations.kt
+++ app/src/main/java/com/geek/rick_and_morty_api/domain/model/Locations.kt
@@ -3,4 +3,5 @@ package com.geek.rick_and_morty_api.domain.model
 data class Locations(
     val name: String?,
     val type: String?,
+    val id: Int?
 )
